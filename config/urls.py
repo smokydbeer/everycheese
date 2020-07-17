2890d866f684c6e9c79aceca11abaee1c593a9fd
Your Name <ericgarcia@questgsm.com>
2020-07-17 11:27:43 -0700

    Implement cheese detail page

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
         include("everycheese.users.urls", namespace="users"),
     ),
     path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
+    path('cheeses', include('everycheese.cheeses.urls', namespace='cheeses'),),
     path('cheeses/', include('everycheese.cheeses.urls', namespace='cheeses'),),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
