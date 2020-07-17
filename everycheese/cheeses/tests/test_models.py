b13f611c8b8b7b6ebe0eb3fadbe16e03ebdbc9a8
Your Name <ericgarcia@questgsm.com>
2020-07-17 11:45:13 -0700

    Use CheeseFactory in a test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -7,6 +7,6 @@ from .factories import CheeseFactory
 
 
 def test__str__():
-    cheese = CheeseFactory(name="Stracchino")
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
